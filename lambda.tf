5666e5318b7282ff3153caa92e499293a9f046eb
juanma2 <juanmanperez@gmail.com>
2022-10-06 11:29:56 +0200

    naming

--- lambda.tf
+++ lambda.tf
@@ -1,10 +1,10 @@
-module "lambda_for_demicon" {
+module "lambda_for_dem" {
   source  = "terraform-aws-modules/lambda/aws"
   version = "4.0.2"
 
-  function_name = "demiconCall"
-  description   = "demicon call to to feed cloudformation"
-  handler       = "demiconCall.lambda_handler"
+  function_name = "demCall"
+  description   = "dem call to to feed cloudformation"
+  handler       = "dem.lambda_handler"
   runtime       = "python3.9"
   source_path   = "${path.module}/lambda"
   
@@ -13,17 +13,17 @@ module "lambda_for_demicon" {
   #liklye some work need to be done to reach the connectivity of from Cloudformation
   # attach_network_policy = true
 
-  layers = [aws_lambda_layer_version.demicon_layer.arn]
+  layers = [aws_lambda_layer_version.dem_layer.arn]
   # create_package         = false
   # local_existing_package = "${path.module}/layer/functions.zip"
 
 }
 
 
-resource "aws_lambda_layer_version" "demicon_layer" {
+resource "aws_lambda_layer_version" "dem_layer" {
   # this zip is generated from a virtualenv as explained here: https://docs.aws.amazon.com/lambda/latest/dg/python-package.html
   filename   = "${path.module}/layer/functions.zip"
-  layer_name = "demicon_terraform_layer"
+  layer_name = "dem_terraform_layer"
 
   compatible_runtimes = ["python3.9"]
 }
